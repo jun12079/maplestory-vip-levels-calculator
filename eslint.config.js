ace1aa3fffcb98f33b589641db75d5e2526edd85
jun12079 <jeffery851007@gmail.com>
2025-05-14 16:55:39 +0800

    chore: 安裝 eslint-plugin-react 來修正class屬性警告
    新版Vite eslint-plugin-react 未預設安裝，導致 'Unknown property 'class' found, use 'className' instead'警告不會顯示

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
@@ -17,17 +18,23 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'indent': ['error', 2, { SwitchCase: 1 }],
+      'no-console': 'error',
     },
   },
 ]
